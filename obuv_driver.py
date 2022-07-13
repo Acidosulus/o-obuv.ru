bd58d695ac7a1c6f7dba97dc18e5d87dd240bf61
Acidosulus <acidos@yandex.ru>
2022-07-13 22:09:48 +0700

    driver ready

--- obuv_driver.py
+++ obuv_driver.py
@@ -12,42 +12,40 @@ import configparser
 from bs4 import BeautifulSoup
 from lxml import html
 import requests
+from click import echo, style
+from fake_useragent import UserAgent
 
 
 class WD:
     def init(self):
-        self.site_url = 'https://kosmetika-krym.ru/'
+        self.site_url = 'https://o-obuv.ru/'
         config = configparser.ConfigParser()
     def __init__(self):
         self.init()
-
 
     def __del__(self):
         try:
             pass
         except: pass
 
-
     def Get_HTML(self, curl):
-        r = requests.get(curl)
+        r = requests.get(curl, headers={'User-Agent': UserAgent().chrome})
         self.page_source = r.text
         return r.text
 
-
     def Get_List_Of_Links_On_Goods_From_Catalog(self, pc_link):
-        print(Fore.RED + 'Список товаров каталога: ' + Fore.YELLOW + pc_link + Fore.RESET)
+        echo(style('Список товаров каталога: ', fg='bright_yellow') + style(pc_link, fg='bright_white'))
         ll_catalog_items = []
         self.Get_HTML(pc_link)
+        self.Write_To_File('catalog.html')
         soup = BeautifulSoup(self.page_source, features='html5lib')
-        items = soup.find_all('div', {'class': 'product-thumb'})
-        print('count:', len(items))
+        items = soup.find_all('a', {'class': 'mainproducts-href'})
         for item in items:
-            hrefs_soup = BeautifulSoup(str(item.contents[1]), features='html5lib')
-            lc_link = hrefs_soup.find('a')['href'].replace('./',self.site_url)
+            lc_link = sx(str(item),'href="','"')
             if lc_link not in ll_catalog_items:
                 ll_catalog_items.append(lc_link)
+        echo(style("Количество товаров:", fg='bright_cyan')+style(len(ll_catalog_items), fg='bright_green'))
         return ll_catalog_items
-
 
     def Write_To_File(self, cfilename):
         file = open(cfilename, "w", encoding='utf-8')
@@ -59,7 +57,7 @@ def Login():
     return WD()
 
 
-colorama.init()
+#colorama.init()
 
 #wd = Login()
-#print(wd.Get_List_Of_Links_On_Goods_From_Catalog('https://kosmetika-krym.ru/catalog/travyanoy_chay_kryima/'))
+#print(wd.Get_List_Of_Links_On_Goods_From_Catalog('https://o-obuv.ru/'))
